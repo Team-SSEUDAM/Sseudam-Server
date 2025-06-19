7d54016a97ea94c0a753996c5c79a87c48dfba14
ybchar <uiurihappy@naver.com>
2025-06-19 22:36:34 +0900

    fix: request user

--- sseudam-core/core-api/src/main/kotlin/com/sseudam/presentation/v1/pet/UserPetController.kt
+++ sseudam-core/core-api/src/main/kotlin/com/sseudam/presentation/v1/pet/UserPetController.kt
@@ -4,6 +4,7 @@ import com.sseudam.pet.UserPetPolicy
 import com.sseudam.pet.UserPetService
 import com.sseudam.presentation.v1.annotation.ApiV1Controller
 import com.sseudam.presentation.v1.pet.response.UserPetInfoResponse
+import com.sseudam.user.User
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,8 +17,8 @@ class UserPetController(
 ) {
     @Operation(summary = "펫 정보 조회", description = "사용자의 펫 정보를 조회합니다.")
     @GetMapping("/pets")
-    fun findUserPetInfo(userId: Long): UserPetInfoResponse {
-        val petInfo = userPetService.findPetInfo(userId)
+    fun findUserPetInfo(user: User): UserPetInfoResponse {
+        val petInfo = userPetService.findPetInfo(user.id)
         val petLevel = userPetPolicy.getLevelType(petInfo.point)
         val maxLevelStandard = userPetPolicy.getMaxLevelStandard(petLevel)
         return UserPetInfoResponse.of(petInfo, petLevel, maxLevelStandard)
