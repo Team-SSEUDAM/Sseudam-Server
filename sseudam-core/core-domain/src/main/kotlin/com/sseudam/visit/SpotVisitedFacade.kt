18136cee9532a0905856eb29b7a29f0778db6922
ybchar <uiurihappy@naver.com>
2025-07-08 16:02:11 +0900

    feat: visited notification

--- sseudam-core/core-domain/src/main/kotlin/com/sseudam/visit/SpotVisitedFacade.kt
+++ sseudam-core/core-domain/src/main/kotlin/com/sseudam/visit/SpotVisitedFacade.kt
@@ -1,11 +1,13 @@
 package com.sseudam.visit
 
 import com.sseudam.notification.FcmSender
+import com.sseudam.notification.NotificationMessages
+import com.sseudam.notification.SendNotificationMessage
 import com.sseudam.pet.PetPointAction
 import com.sseudam.pet.event.PetEventPublisher
 import com.sseudam.suggestion.SuggestionService
 import com.sseudam.trashspot.TrashSpotService
-import com.sseudam.user.device.UserDeviceService
+import com.sseudam.user.UserService
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,19 +16,26 @@ class SpotVisitedFacade(
     private val trashSpotService: TrashSpotService,
     private val petEventPublisher: PetEventPublisher,
     private val suggestionService: SuggestionService,
-    private val userDeviceService: UserDeviceService,
+    private val userService: UserService,
     private val fcmSender: FcmSender,
 ) {
     fun visitSpot(
         userId: Long,
         spotId: Long,
     ) {
-        val visited = spotVisitedService.append(SpotVisited.Create(userId, spotId))
+        val spot = trashSpotService.findBy(spotId)
+        val visited = spotVisitedService.append(SpotVisited.Create(userId, spot.id))
         petEventPublisher.publish(visited.userId, PetPointAction.SPOT_VISITED)
 
-        val suggestion = suggestionService.findSpotSuggestionBySpotId(spotId)
-
-        val userDevices = userDeviceService.findByUserId(visited.userId)
+        val suggestion = suggestionService.findSpotSuggestionBySite(spot.address.site) ?: return
+        val profile = userService.getProfile(suggestion.userId)
+        fcmSender.send(
+            SendNotificationMessage(
+                userId = suggestion.userId,
+                title = NotificationMessages.DEFAULT_TITLE,
+                body = NotificationMessages.anonymousVisitedSpotContents(profile.nickname),
+            ),
+        )
     }
 
     fun findSpotVisitedByUserId(userId: Long): SpotVisitedAll {
