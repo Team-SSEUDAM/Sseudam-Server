4949708344abd88212b3d8f6d674c5cd699c0020
ybchar <uiurihappy@naver.com>
2025-07-11 15:31:48 +0900

    fix: modulith events jpa path

--- sseudam-storage/db-core/src/main/kotlin/com/sseudam/storage/db/core/config/CoreJpaConfig.kt
+++ sseudam-storage/db-core/src/main/kotlin/com/sseudam/storage/db/core/config/CoreJpaConfig.kt
@@ -11,14 +11,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
     basePackages = [
         "com.sseudam.storage.db.core",
         "com.sseudam.admin.infrastructure",
-        "org.springframework.modulith.events.jpa.*",
+        "org.springframework.modulith.events.jpa",
     ],
 )
 @EnableJpaRepositories(
     basePackages = [
         "com.sseudam.storage.db.core",
         "com.sseudam.admin.infrastructure",
-        "org.springframework.modulith.events.jpa.*",
+        "org.springframework.modulith.events.jpa",
     ],
 )
 class CoreJpaConfig
