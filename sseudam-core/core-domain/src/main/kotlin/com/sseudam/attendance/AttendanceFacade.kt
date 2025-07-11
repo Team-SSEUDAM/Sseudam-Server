8ab41e8ba64c01545f4bbfc165c3e4e29c0fbd77
ybchar <uiurihappy@naver.com>
2025-07-11 10:23:56 +0900

    refactor: attendance continuity point

--- sseudam-core/core-domain/src/main/kotlin/com/sseudam/attendance/AttendanceFacade.kt
+++ sseudam-core/core-domain/src/main/kotlin/com/sseudam/attendance/AttendanceFacade.kt
@@ -1,37 +1,25 @@
 package com.sseudam.attendance
 
 import com.sseudam.pet.PetPointAction
-import com.sseudam.pet.PetPointHistoryService
-import com.sseudam.pet.UserPetService
 import com.sseudam.pet.event.PetEventPublisher
-import com.sseudam.support.tx.TxAdvice
 import org.springframework.stereotype.Service
 
 @Service
 class AttendanceFacade(
     private val attendanceService: AttendanceService,
-    private val userPetService: UserPetService,
-    private val petPointHistoryService: PetPointHistoryService,
     private val petEventPublisher: PetEventPublisher,
-    private val txAdvice: TxAdvice,
 ) {
-    fun todayAttendance(userId: Long): Pair<Boolean, Attendance.Complete> =
-        txAdvice.write {
-            val attendance = attendanceService.attendance(userId)
-
-            if (!attendance.second.isToday) {
-                if (attendance.second.continuity == 5) {
-                    val bonusAction = PetPointAction.BONUS_ATTENDANCE
-                    val userPet = userPetService.findByUser(userId)
-                    userPet?.let {
-                        userPetService.updatePointByAction(it, bonusAction)
-                        petPointHistoryService.append(it, bonusAction)
-                    }
+    fun todayAttendance(userId: Long): Pair<Boolean, Attendance.Complete> {
+        val (isContinuity, complete) = attendanceService.attendance(userId)
+        if (!complete.isToday) {
+            val action =
+                if (complete.continuity == 5) {
+                    PetPointAction.CONTINUITY_ATTENDANCE
+                } else {
+                    PetPointAction.ATTENDANCE
                 }
-
-                petEventPublisher.publish(userId, PetPointAction.ATTENDANCE)
-            }
-
-            return@write attendance
+            petEventPublisher.publish(userId, action)
         }
+        return Pair(isContinuity, complete)
+    }
 }
