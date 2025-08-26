7c74958e8b9545b214971017df2e0e76aba90a08
ybchar <uiurihappy@naver.com>
2025-08-26 21:58:10 +0900

    docs: DocsTag

--- sseudam-tests/api-docs/src/main/kotlin/com/sseudam/docs/support/DocsTag.kt
+++ sseudam-tests/api-docs/src/main/kotlin/com/sseudam/docs/support/DocsTag.kt
@@ -3,7 +3,13 @@ package com.sseudam.docs.support
 enum class DocsTag(
     val tagName: String,
 ) {
-    /** New */
-    AUTH("🔑 Authentication API"),
+    AUTH("🔑 Auth API"),
     USER("🧍🏻 User API"),
+    TRASH_SPOT("🗑️ Trash Spot API"),
+    VISITED("🙋 Visited API"),
+    SUGGESTION("🤝 Suggestion API"),
+    REPORT("🚨 Report API"),
+    VERSION("📱 Version API"),
+    PET("😽 Pet API"),
+    HISTORY("📑 History API"),
 }
