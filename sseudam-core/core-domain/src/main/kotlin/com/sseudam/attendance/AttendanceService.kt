3818c734e97298b14cd6085a59acaa54de7edd3f
ybchar <uiurihappy@naver.com>
2025-08-27 13:35:44 +0900

    [Hotfix] Attendance isToday

--- sseudam-core/core-domain/src/main/kotlin/com/sseudam/attendance/AttendanceService.kt
+++ sseudam-core/core-domain/src/main/kotlin/com/sseudam/attendance/AttendanceService.kt
@@ -11,9 +11,12 @@ class AttendanceService(
     fun attendance(userId: Long): Triple<Boolean, Attendance.Complete, Boolean> {
         val currentDate = LocalDate.now()
         val attendance = attendanceReader.readLastByUser(userId)
-        val isFirstAttendanceToday = attendance?.date != currentDate
+        println("attendacne: $attendance $currentDate")
+        val isFirstAttendanceToday = attendance?.date == currentDate
 
-        if (attendance == null || isFirstAttendanceToday) {
+        println("isFirstAttendanceToday: $isFirstAttendanceToday")
+
+        if (attendance == null || !isFirstAttendanceToday) {
             val continuity =
                 if (attendance?.date?.plusDays(1) == currentDate) {
                     ((attendance?.continuity ?: 0) + 1).coerceAtMost(5)
@@ -24,14 +27,14 @@ class AttendanceService(
             return Triple(
                 continuity > 1,
                 newAttendance.toComplete(isToday = isFirstAttendanceToday),
+                isFirstAttendanceToday,
+            )
+        } else {
+            return Triple(
+                attendance.continuity > 1,
+                attendance.toComplete(isToday = true),
                 true,
             )
         }
-
-        return Triple(
-            attendance.continuity > 1,
-            attendance.toComplete(isToday = true),
-            false,
-        )
     }
 }
