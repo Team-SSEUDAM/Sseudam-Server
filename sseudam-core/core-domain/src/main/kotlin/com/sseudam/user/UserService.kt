803c5e005137cc70785f12e55455fcfd3b92e6d0
ybchar <uiurihappy@naver.com>
2025-09-06 15:26:16 +0900

    fix: withdrawal re-signup

--- sseudam-core/core-domain/src/main/kotlin/com/sseudam/user/UserService.kt
+++ sseudam-core/core-domain/src/main/kotlin/com/sseudam/user/UserService.kt
@@ -1,9 +1,15 @@
 package com.sseudam.user
 
 import com.sseudam.common.Address
+import com.sseudam.pet.Pet
+import com.sseudam.pet.PetReader
+import com.sseudam.pet.UserPetAppender
 import com.sseudam.support.cursor.OffsetPageRequest
+import com.sseudam.support.error.ErrorException
+import com.sseudam.support.error.ErrorType
 import com.sseudam.support.page.Page
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class UserService(
@@ -12,10 +18,19 @@ class UserService(
     private val userUpdater: UserUpdater,
     private val userDeleter: UserDeleter,
     private val userValidator: UserValidator,
+    private val userPetAppender: UserPetAppender,
+    private val petReader: PetReader,
 ) {
     fun create(newUser: NewUser): User {
         userValidator.verifyEmail(newUser.email)
-        return userAppender.create(newUser)
+        val (currentYear, currentMonth) = LocalDate.now().let { it.year to it.month }
+        val pets = petReader.readAllLatestSeasonPets(currentYear, currentMonth)
+        val level1Pet =
+            pets.find { it.levelType == Pet.LevelType.LEVEL_1 }
+                ?: throw ErrorException(ErrorType.INVALID_PET_LEVEL_TYPE)
+        val createUser = userAppender.create(newUser)
+        userPetAppender.append(createUser.id, level1Pet)
+        return createUser
     }
 
     fun getProfile(userId: Long): UserProfile? = userReader.readUserProfile(userId)
