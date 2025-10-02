d13846e9b63d9175f3e0f5ca2f5cfda5e28c6a09
ybchar <uiurihappy@naver.com>
2025-10-02 17:06:19 +0900

    chore: core-application module

---
+++ sseudam-core/core-application/build.gradle.kts
@@ -0,0 +1,34 @@
+
+dependencies {
+    api(libs.kotlin.logging)
+    compileOnly(libs.spring.context)
+    implementation(project(":sseudam-core:core-contract"))
+
+    // Coroutine
+    implementation(libs.kotlinx.coroutine.core)
+    implementation(libs.kotlinx.coroutine.reactor)
+    implementation(libs.kotlinx.coroutine.slf4j)
+    implementation(libs.reactor.kotlin)
+    // Arrow Kt
+    implementation(libs.arrow.fx.coroutine)
+    implementation(libs.arrow.fx.stm)
+
+    implementation(libs.spring.tx)
+    implementation(libs.slf4j)
+
+    // modulith
+    implementation(libs.bundles.spring.modulith)
+    runtimeOnly(libs.bundles.spring.modulith.runtime)
+
+    testImplementation(libs.bundles.spring.modulith.tests)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(
+            libs.spring.modulith.bom
+                .get()
+                .toString(),
+        )
+    }
+}
