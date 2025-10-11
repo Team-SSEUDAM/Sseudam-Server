56af7704209d3bfb4f1cda9b242f22406ff0ccb4
yb__char <68099546+char-yb@users.noreply.github.com>
2025-10-11 12:31:25 +0900

    [Feature] Add Apns destination CustomData  (#263)
    * feat: apns custom data, destination
    
    * fix: destination

--- sseudam-core/core-application/src/main/kotlin/com/sseudam/visit/event/VisitedEventHandler.kt
+++ sseudam-core/core-application/src/main/kotlin/com/sseudam/visit/event/VisitedEventHandler.kt
@@ -56,6 +56,7 @@ class VisitedEventHandler(
                         userId = suggestion.userId,
                         title = NotificationMessages.DEFAULT_TITLE,
                         body = NotificationMessages.anonymousVisitedSpotContents(profile.nickname),
+                        destination = "MyPageView",
                     ),
                 type = NotificationType.ANONYMOUS_VISITED_SPOT,
                 parameterValue = event.spot.id.toString(),
