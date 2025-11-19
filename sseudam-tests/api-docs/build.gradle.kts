3948dc0a20eefff9ba1d9d6ab0816d516f2c3c1d
ybchar <uiurihappy@naver.com>
2025-11-20 00:26:43 +0900

    fix: restdocs 4.0.0

--- sseudam-tests/api-docs/build.gradle.kts
+++ sseudam-tests/api-docs/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly(libs.jakarta.servlet.api)
     compileOnly(libs.spring.boot.starter.test)
-    api(libs.spring.restdocs.mockmvc)
+    api(libs.bundles.spring.restdocs)
     api(libs.restassured.spring.mock.mvc)
     api(libs.epages.restdocs.api.spec.mock.mvc)
     // Jackson 3.0에서 jackson-datatype-jsr310은 jackson-databind에 통합됨
