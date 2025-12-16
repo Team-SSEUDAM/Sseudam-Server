876415dbae5e04e9049f32d82fd123651eea3159
ybchar <uiurihappy@naver.com>
2025-12-17 02:41:31 +0900

    fix: developTest EnableConfiguration

--- sseudam-core/core-api/src/test/kotlin/com/sseudam/DevelopTest.kt
+++ sseudam-core/core-api/src/test/kotlin/com/sseudam/DevelopTest.kt
@@ -1,19 +1,12 @@
 package com.sseudam
 
-import io.sentry.spring.boot.jakarta.SentryAutoConfiguration
 import org.junit.jupiter.api.Tag
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestConstructor
 
 @Tag("develop")
 @SpringBootTest
-@EnableAutoConfiguration(
-    exclude = [
-        SentryAutoConfiguration::class,
-    ],
-)
 @ContextConfiguration(classes = [RedisContainersConfig::class, TestTxConfig::class])
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 annotation class DevelopTest
